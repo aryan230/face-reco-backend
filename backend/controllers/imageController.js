aab112664301a56016c8243a90b2d23304d8180a
aryan230 <aryan23062001@gmail.com>
2023-02-21 14:00:30 +0530

    second

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -2,6 +2,7 @@ import Image from "../models/imageModel.js";
 import asyncHandler from "express-async-handler";
 
 const saveImage = asyncHandler(async (req, res) => {
+  console.log(req);
   const { image } = req.body;
 
   const setImage = await Image.create({
